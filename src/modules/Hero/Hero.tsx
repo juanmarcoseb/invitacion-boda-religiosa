9626ccbf5bc63c433eff39ee3900d7303a55c7c5
Marcos Escobar <68478662+juanmarcosescobar@users.noreply.github.com>
2025-12-21 20:01:14 -0600

    Add Home page and first Hero module

---
+++ src/modules/Hero/Hero.tsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <section style={{ minHeight: "100vh" }}>
+      <h1>Juan & Marcos</h1>
+      <p>Nuestra Boda</p>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Hero from "../modules/Hero/Hero"
+
+const Home = () => {
+  return (
+    <>
+      {/* Aquí irán los módulos */}
+      <Hero />
+    </>
+  )
+}
+
+export default Home
