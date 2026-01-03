7476917bbba0caceda782dde23971dd675daa86e
Marcos Escobar <68478662+juanmarcosescobar@users.noreply.github.com>
2026-01-02 23:59:29 -0600

    feat(sobre) Modulo de invitacion

--- src/modules/Carousel/Carousel.tsx
+++ src/modules/Carousel/Carousel.tsx
@@ -1,15 +1,15 @@
 import "./Carousel.css"
 
-import img1 from "../../assets/carousel/photo-01.jpg"
-import img2 from "../../assets/carousel/photo-02.jpg"
-import img3 from "../../assets/carousel/photo-03.jpg"
-import img4 from "../../assets/carousel/photo-04.jpg"
-import img5 from "../../assets/carousel/photo-05.jpg"
-import img6 from "../../assets/carousel/photo-06.jpg"
-import img7 from "../../assets/carousel/photo-07.jpg"
-import img8 from "../../assets/carousel/photo-08.jpg"
-import img9 from "../../assets/carousel/photo-09.jpg"
-import img10 from "../../assets/carousel/photo-10.jpg"
+import img1 from "../../assets/carousel/photo-01.webp"
+import img2 from "../../assets/carousel/photo-02.webp"
+import img3 from "../../assets/carousel/photo-03.webp"
+import img4 from "../../assets/carousel/photo-04.webp"
+import img5 from "../../assets/carousel/photo-05.webp"
+import img6 from "../../assets/carousel/photo-06.webp"
+import img7 from "../../assets/carousel/photo-07.webp"
+import img8 from "../../assets/carousel/photo-08.webp"
+import img9 from "../../assets/carousel/photo-09.webp"
+import img10 from "../../assets/carousel/photo-10.webp"
 
 const images = [
   img1, img2, img3, img4, img5,
